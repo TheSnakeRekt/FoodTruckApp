27ff9111391c57cf5f834fdd139d2e6697f6afc1
Jorge Guimaraes <j.guimaraes@cogia.de>
2024-02-19 00:51:40 +0000

    feat: cleanup route and controller

--- backend/src/controllers/authenticationController.ts
+++ backend/src/controllers/authenticationController.ts
@@ -1,12 +1,28 @@
 import { AuthenticationService } from "../services/authenticationService";
 import { Service } from "typedi";
 import "reflect-metadata";
+import { Request, Response } from "express";
 
 @Service()
 export class AuthenticationController {
   constructor(private authenticationService: AuthenticationService) {}
 
-  async authenticate({ username, password }: any) {
-    this.authenticationService.authenticate(username, password);
+  async authenticate(req: Request, res: Response) {
+    const { username, password } = req.body;
+    const data = await this.authenticationService.authenticate(
+      username,
+      password
+    );
+    if (data !== null) {
+      res.status(200).send(data);
+    } else {
+      res.status(404).send(data);
+    }
   }
+
+  currentAuth = (req: Request, res: Response) => {
+    if (req.user !== null) {
+      res.status(200).send(req.user);
+    }
+  };
 }
